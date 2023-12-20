a219a5999203f8223be14415d61298712494a0c6
Mou Xiangyu <xiangyu.moe@gmail.com>
2023-12-20 19:56:01 +0800

    build some possible models

--- model/my_transforms.py
+++ model/my_transforms.py
@@ -5,8 +5,9 @@ import torchvision
 transform = [
     torchvision.transforms.Compose([
         transforms.Resize((128, 171)),
-        transforms.RandomCrop((112, 112)),
-        transforms.RandomRotation(10),
+        # transforms.RandomCrop((112, 112)),
+        # transforms.RandomRotation(10),
+        transforms.CenterCrop((112, 112)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225]),
